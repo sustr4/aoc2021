4799c249fd63d73d363aec2a757e0b458b85bd11
Zdenek Sustr <git@sustr.net>
2021-12-13 11:10:55 +0100

    Reduce code

--- day13.sh
+++ day13.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-cat day13-input.txt | grep "fold along y=" | awk -F"=" 'BEGIN {print "fixy () {\n\tRET=$1"} {print "\tif [ $RET -gt " $2 " ]; then RET=$((" $2 "+" $2 "-$RET)); fi" } END { print "\tprintf $RET\n}"}' > day13-fix.sh
-cat day13-input.txt | grep "fold along x=" | awk -F"=" 'BEGIN {print "fixx () {\n\tRET=$1"} {print "\tif [ $RET -gt " $2 " ]; then RET=$((" $2 "+" $2 "-$RET)); fi" } END { print "\tprintf $RET\n}"}' >> day13-fix.sh
+for COORD in x y; do
+	cat day13-input.txt | grep "fold along ${COORD}=" | awk -v coord="${COORD}" -F"=" 'BEGIN {print "fix" coord " () {\n\tRET=$1"} {print "\tif [ $RET -gt " $2 " ]; then RET=$((" $2 "+" $2 "-$RET)); fi" } END { print "\tprintf $RET\n}"}'; done > day13-fix.sh
 
 source day13-fix.sh
 
 cat day13-input.txt | egrep -E '^[0-9]' | while IFS=, read -r x y; do
-	fixx $x
-	printf " -"
-	fixy $y
-	printf "\n"
+	printf "`fixx $x` -`fixy $y`\n"
 done > day13.dat
 
 gnuplot -c day13.plt 
